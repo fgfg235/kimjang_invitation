458e2a83d0b9c45023693f3f72caf3db3fdb5029
kgp_tmp <fgfg235@gmail.com>
2024-10-19 05:49:31 +0900

    dist commit 34

--- assets/index-1R7YygfF.js
+++ assets/index-1R7YygfF.js
@@ -2821,13 +2821,16 @@ This typically indicates that your device does not have a healthy Internet conne
   }
 
   .message-form {
-    max-width: 500px;
+    max-width: 600px;
     margin: 0 auto;
     padding: 20px;
     background-color: ${tt.light};
     border-radius: 8px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     text-align: center;
+    @media (max-width: 599px) {
+      max-width: 400px;
+    }
   }
 
   .field {
